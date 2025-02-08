521fa72441222ab1401f6b4e44af5bb96dad808a
Tuff <38067341+tuffaholicc@users.noreply.github.com>
2025-02-09 00:26:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const slotContainer = document.getElementById("slotContainer");
 
-  // Generate 3x20 = 60 slots dynamically
-  for (let i = 1; i <= 60; i++) {
+  // Generate 3x5 = 15 slots dynamically
+  for (let i = 1; i <= 15; i++) {
     const slot = document.createElement("div");
     slot.classList.add("slot");
 
     const iconImage = document.createElement("img");
     iconImage.classList.add("icon-image");
-    iconImage.src = `icons/icon${i}.png`; // Assumes icon files are named as icon1.png, icon2.png, etc.
+    iconImage.src = `icons/icon${i}.png`; // Assumes icon files are named icon1.png, icon2.png, etc.
     iconImage.alt = `Icon ${i}`;
 
     const iconName = document.createElement("div");
